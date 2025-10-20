dc84a222e749e983ae03971f5141bc4a806b2603
ArezouFarshi <arezou.farshi@mail.polimi.it>
2025-10-20 23:36:13 +0200

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -7,7 +7,7 @@
   const toBlockEl = $("toBlock");
   const rpcUrlEl = $("rpcUrl");
   const contractEl = $("contractAddress");
-  const backendEl = $("backendBase"); // hidden field, but still in DOM!
+  const backendEl = $("backendBase");
   const jsonOut = $("jsonOut");
   const jsonLink = $("jsonLink");
   const hashLink = $("hashLink");
@@ -46,8 +46,9 @@
     try {
       const provider = new ethers.JsonRpcProvider(rpcUrl);
       const iface = new ethers.Interface([`event ${EVENT_SIG}`]);
-      // ETHERS v6+ topic hash:
+      // ----------- FIXED LINE FOR ETHERS v6+ -------------
       const topic0 = iface.getEvent("PanelEventAdded").topicHash;
+      // ----------------------------------------------------
 
       let fromBlock = fromBlockEl.value ? Number(fromBlockEl.value) : 0;
       let toBlock = toBlockEl.value ? Number(toBlockEl.value) : await provider.getBlockNumber();
