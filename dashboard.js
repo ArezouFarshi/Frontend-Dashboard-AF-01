444523429b7e907286f4e6d0d7ecc27806b347b0
ArezouFarshi <arezou.farshi@mail.polimi.it>
2025-11-18 22:21:47 +0100

    Update RPC_URL to use Alchemy service

--- dashboard.js
+++ dashboard.js
@@ -2,7 +2,7 @@
   const CONFIG = {
     BACKEND_BASE: "https://dpp-update-frontend-af02.onrender.com",
     ACCESS_DEFAULT: "public",
-    RPC_URL: "https://sepolia.infura.io/v3/57ea67cde27f45f9af5a69bdc5c92332",
+    RPC_URL: "https://eth-sepolia.g.alchemy.com/v2/HtuAd38udm_ZT9rD4vttNJrbKhP-CUMF",
     CONTRACT_ADDRESS: "0xF2dCCAddE9dEe3ffF26C98EC63e2c44E08B4C65c",
     EVENT_SIG: "PanelEventAdded(string,bool,string,string,int256,string,uint256)"
   };
